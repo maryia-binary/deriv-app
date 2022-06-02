48e8c29bf96ab9f960e86075d1cff2ad313960e6
maryia-binary <maryia@binary.com>
2022-06-02 09:55:55 +0300

    Maryia/added dummy accumulators data to  render accumulators in contract purchase window for Vol25(1s)Index

--- packages/trader/src/Stores/Modules/Trading/Constants/contract.js
+++ packages/trader/src/Stores/Modules/Trading/Constants/contract.js
@@ -4,6 +4,7 @@ import { shouldShowCancellation, shouldShowExpiration } from '@deriv/shared';
 export const getLocalizedBasis = () => ({
     payout: localize('Payout'),
     stake: localize('Stake'),
+    growth_frequency: localize('Accumulator'),
     multiplier: localize('Multiplier'),
 });
 
@@ -103,6 +104,14 @@ export const getContractTypesConfig = symbol => ({
         basis: [],
         components: [],
     },
+    accumulator: {
+        title: localize('Accumulators'),
+        trade_types: ['ACC'],
+        basis: ['stake', 'growth_frequency'],
+        components: ['take_profit', 'barrier'],
+        barrier_count: 2,
+        config: { hide_duration: true },
+    },
     multiplier: {
         title: localize('Multipliers'),
         trade_types: ['MULTUP', 'MULTDOWN'],
@@ -119,6 +128,7 @@ export const getContractTypesConfig = symbol => ({
 
 export const getContractCategoriesConfig = () => ({
     [localize('Multipliers')]: ['multiplier'],
+    [localize('Accumulators')]: ['accumulator'],
     [localize('Ups & Downs')]: ['rise_fall', 'rise_fall_equal', 'run_high_low', 'reset', 'asian', 'callputspread'],
     [localize('Highs & Lows')]: ['high_low', 'touch', 'tick_high_low'],
     [localize('Ins & Outs')]: ['end', 'stay'],

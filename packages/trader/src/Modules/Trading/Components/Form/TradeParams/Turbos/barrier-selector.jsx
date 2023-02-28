d1e717bba169b24363e2c291c09f75794ec4df28
kate-deriv <kate@regentmarkets.com>
2023-02-28 10:40:55 +0300

    refactor: replace the rest of the text by Text component

--- packages/trader/src/Modules/Trading/Components/Form/TradeParams/Turbos/barrier-selector.jsx
+++ packages/trader/src/Modules/Trading/Components/Form/TradeParams/Turbos/barrier-selector.jsx
@@ -78,7 +78,9 @@ const BarrierSelector = ({ barrier_1, onChange, setHoveredBarrier, turbos_barrie
 
     const barriers_list = (
         <React.Fragment>
-            <div className='trade-container__barriers-table__text'>Distance to spot</div>
+            <Text size='xxs' color='disabled' line_height='l' className='trade-container__barriers-table__text'>
+                Distance to spot
+            </Text>
             <ThemedScrollbars height={isMobile() ? 'calc(100% - 5.5rem)' : null} autohide={false}>
                 <BarriersList
                     base_classname='trade-container__barriers-table__item'

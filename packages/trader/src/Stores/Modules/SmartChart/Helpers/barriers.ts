2a88d0e12e47fdb06079bc4ec7744cd25185de7c
maryia-deriv <maryia@binary.com>
2023-06-28 17:21:25 +0300

    feat: migrated chart-barrier-store.js to ts

--- packages/trader/src/Stores/Modules/SmartChart/Helpers/barriers.ts
+++ packages/trader/src/Stores/Modules/SmartChart/Helpers/barriers.ts
@@ -1,25 +1,11 @@
-export type TBarrier = {
-    key?: string;
-    title?: string;
-    color: string;
-    draggable: boolean;
-    hidePriceLines: boolean;
-    hideOffscreenLine?: boolean;
-    showOffscreenArrows?: boolean;
-    isSingleBarrier: boolean;
-    opacityOnOverlap?: number;
-    lineStyle: string;
-    hideBarrierLine: boolean;
-    shade?: string;
-    shadeColor?: string;
-    high: string | number;
-    low?: string | number;
-    relative: boolean;
-    onChange: (barriers: { high: string | number; low: string | number }) => void;
-    onChartBarrierChange: null | ((barrier_1: string, barrier_2: string) => void);
-};
+import type { ChartBarrierStore } from '../chart-barrier-store';
 
-export const barriersToString = (is_relative: boolean, ...barriers_list: Array<string | number>) =>
+export type TBarrier = ChartBarrierStore & { key?: string };
+
+export const barriersToString = (
+    is_relative: boolean,
+    ...barriers_list: Array<string | number | undefined>
+): Array<string | undefined> =>
     barriers_list
         .filter(barrier => barrier !== undefined && barrier !== null)
         .map(barrier => `${is_relative && !/^[+-]/.test(`${barrier}`) ? '+' : ''}${barrier}`);

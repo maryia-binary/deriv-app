11647a7eed5b296607ae1fd8890ba2f33b5f730f
maryia-deriv <maryia@binary.com>
2023-11-27 15:29:04 +0300

    Updates

---
+++ js/core.6408.a7ce8111f97a47bc0d3e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[6408],{96408:(e,s,k)=>{k.r(s)}}]);
---
+++ js/core.8281.75100522816bcb2ac4fb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[8281],{78281:(e,s,k)=>{k.r(s)}}]);

f30eb0b4f63d1e4f495533fb41e4a7716d11be3a
maryia-deriv <maryia@binary.com>
2024-07-11 17:45:02 +0300

    feat: add display components for Rise/Fall based on real data + config for trade params

--- packages/trader/src/AppV2/Components/TradeParameters/trade-parameters.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/trade-parameters.tsx
@@ -1,39 +1,87 @@
 import React from 'react';
 import clsx from 'clsx';
-import { TextField, Text } from '@deriv-com/quill-ui';
+import { Text } from '@deriv-com/quill-ui';
 import Guide from '../Guide';
+import { observer } from 'mobx-react';
+import { useTraderStore } from 'Stores/useTraderStores';
+import AllowEquals from './AllowEquals';
+import { getTradeParams } from 'AppV2/Utils/trade-params-utils';
+import Duration from './Duration';
+import Stake from './Stake';
 
 type TTradeParameters = {
     is_minimized?: boolean;
-    trade_parameters_list: { label: React.ReactNode; value: string | number }[];
 };
 
-const TradeParameters = ({ is_minimized, trade_parameters_list }: TTradeParameters) => (
-    <section className={clsx('trade-params', is_minimized && 'trade-params--minimized')}>
-        {!is_minimized && (
-            <div className='trade-params__title'>
-                <Text>Set your trade</Text>
-                <Guide has_label />
-            </div>
-        )}
-        <div
-            className={clsx(
-                'trade-params__options__wrapper',
-                is_minimized && 'trade-params__options__wrapper--minimized'
+const TradeParameters = observer(({ is_minimized }: TTradeParameters) => {
+    const {
+        amount,
+        basis,
+        contract_start_type,
+        contract_type,
+        contract_types_list,
+        currency,
+        duration,
+        duration_unit,
+        expiry_type,
+        is_equal,
+        onChange,
+        symbol,
+    } = useTraderStore();
+    const isVisible = (component_key: string) => {
+        return getTradeParams(symbol)[contract_type].includes(component_key);
+    };
+
+    return (
+        <section className={clsx('trade-params', is_minimized && 'trade-params--minimized')}>
+            {!is_minimized && (
+                <div className='trade-params__title'>
+                    <Text>Set your trade</Text>
+                    <Guide has_label />
+                </div>
             )}
-        >
-            {trade_parameters_list.map(({ label, value }) => (
-                <TextField
-                    variant='fill'
-                    readOnly
-                    label={label}
-                    value={value}
-                    key={value}
-                    className={clsx('trade-params__option', is_minimized && 'trade-params__option--minimized')}
-                />
-            ))}
-        </div>
-    </section>
-);
+            <div
+                className={clsx(
+                    'trade-params__options__wrapper',
+                    is_minimized && 'trade-params__options__wrapper--minimized'
+                )}
+            >
+                {/* {isVisible('trade_type_tabs') && <TradeTypeTabs />} */}
+                {/* {isVisible('last_digit') && <LastDigit />} */}
+                {isVisible('duration') && (
+                    <Duration duration={duration} duration_unit={duration_unit} is_minimized={is_minimized} />
+                )}
+                {/* {isVisible('strike') && <Strike />} */}
+                {/* {isVisible('payout_per_point') && <PayoutPerPointSelector />} */}
+                {/* {isVisible('barrier') && <Barrier />} */}
+                {/* {isVisible('growth_rate') && <GrowthRate />} */}
+                {isVisible('stake') && (
+                    <Stake
+                        amount={amount}
+                        basis={basis}
+                        currency={currency}
+                        onChange={onChange}
+                        is_minimized={is_minimized}
+                    />
+                )}
+                {isVisible('allow_equals') && (
+                    <AllowEquals
+                        contract_start_type={contract_start_type}
+                        contract_types_list={contract_types_list}
+                        duration_unit={duration_unit}
+                        expiry_type={expiry_type}
+                        is_minimized={is_minimized}
+                        is_equal={is_equal}
+                    />
+                )}
+                {/* {isVisible('take_profit') && <TakeProfit />} */}
+                {/* {isVisible('risk_management') && <RiskManagement />} */}
+                {/* {isVisible('expiration') && <Expiration />} */}
+                {/* {isVisible('accu_info_display') && <AccumulatorsInfoDisplay />} */}
+                {/* {isVisible('mult_info_display') && <MultipliersInfoDisplay />} */}
+            </div>
+        </section>
+    );
+});
 
 export default TradeParameters;

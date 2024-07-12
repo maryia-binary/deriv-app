9797b4fa7f4265e6ca81b170b53b24bdefe78538
kate-deriv <kate@regentmarkets.com>
2024-07-12 16:31:00 +0300

    refactor: customize amount of buttons and it name

--- packages/trader/src/AppV2/Components/PurchaseButton/purchase-button.tsx
+++ packages/trader/src/AppV2/Components/PurchaseButton/purchase-button.tsx
@@ -1,35 +1,89 @@
 import React from 'react';
+import clsx from 'clsx';
 import { Button } from '@deriv-com/quill-ui';
-import { Localize, localize } from '@deriv/translations';
+import { localize } from '@deriv/translations';
+import { observer } from 'mobx-react';
+// import { useStore } from '@deriv/stores';
+import { useTraderStore } from 'Stores/useTraderStores';
+import { getContractTypeDisplay } from '@deriv/shared';
 
-// TODO: add Text component from Quill UI when it will support sm size with font-size 12px
-const PurchaseButton = () => (
-    <div className='purchase-button__wrapper'>
-        <Button
-            color='purchase'
-            size='lg'
-            label={<Localize i18n_default_text='Rise' />}
-            fullWidth
-            className='purchase-button'
-        >
-            <p className='purchase-button__payout'>
-                <span>{localize('Payout')}</span>
-                <span>19.55 USD</span>
-            </p>
-        </Button>
-        <Button
-            color='sell'
-            size='lg'
-            label={<Localize i18n_default_text='Fall' />}
-            fullWidth
-            className='purchase-button'
-        >
-            <p className='purchase-button__payout'>
-                <span>19.55 USD</span>
-                <span>{localize('Payout')}</span>
-            </p>
-        </Button>
-    </div>
-);
+const PurchaseButton = observer(() => {
+    // const {
+    //     portfolio: { all_positions, onClickSell },
+    //     ui: { purchase_states: purchased_states_arr, is_mobile, setPurchaseState },
+    // } = useStore();
+    const {
+        // basis,
+        contract_type,
+        // currency,
+        // growth_rate,
+        // has_cancellation,
+        // has_open_accu_contract,
+        // is_accumulator,
+        // is_multiplier,
+        // is_purchase_enabled,
+        // is_trade_enabled,
+        // is_turbos,
+        // is_vanilla_fx,
+        // is_vanilla,
+        // onHoverPurchase,
+        // onPurchase: onClickPurchase,
+        // proposal_info,
+        // purchase_info,
+        // symbol,
+        trade_types,
+        // validation_errors,
+    } = useTraderStore();
+
+    const is_high_low = /^high_low$/.test(contract_type.toLowerCase());
+    const trade_types_array = Object.keys(trade_types);
+
+    if (trade_types_array.length === 1) {
+        return (
+            <div className='purchase-button__wrapper'>
+                <Button
+                    color='purchase'
+                    size='lg'
+                    label={getContractTypeDisplay(trade_types_array[0], {
+                        isHighLow: is_high_low,
+                        showButtonName: true,
+                    })}
+                    fullWidth
+                    className='purchase-button purchase-button--single'
+                >
+                    <p className='purchase-button__information__wrapper purchase-button__information__wrapper--single'>
+                        <span>{localize('Payout')}</span>
+                        <span>19.55 USD</span>
+                    </p>
+                </Button>
+            </div>
+        );
+    }
+
+    return (
+        <div className='purchase-button__wrapper'>
+            {trade_types_array.map((trade_type, index) => (
+                <Button
+                    key={trade_type}
+                    color={index ? 'purchase' : 'sell'}
+                    size='lg'
+                    label={getContractTypeDisplay(trade_type, { isHighLow: is_high_low, showButtonName: true })}
+                    fullWidth
+                    className='purchase-button'
+                >
+                    <p
+                        className={clsx(
+                            'purchase-button__information__wrapper',
+                            index && 'purchase-button__information__wrapper--reverse'
+                        )}
+                    >
+                        <span>{localize('Payout')}</span>
+                        <span>19.55 USD</span>
+                    </p>
+                </Button>
+            ))}
+        </div>
+    );
+});
 
 export default PurchaseButton;

174b733ec570e8c9ec6cc053cb5a02d2634bc82e
kate-deriv <kate@regentmarkets.com>
2024-07-18 15:12:04 +0300

    feat: add carousel

--- packages/trader/src/AppV2/Components/TradeParameters/AllowEquals/allow-equals.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/AllowEquals/allow-equals.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import clsx from 'clsx';
 import { ActionSheet, ToggleSwitch, Text, TextField } from '@deriv-com/quill-ui';
-import { LabelPairedCircleInfoMdRegularIcon } from '@deriv/quill-icons';
+import { LabelPairedArrowLeftMdRegularIcon, LabelPairedCircleInfoMdRegularIcon } from '@deriv/quill-icons';
 import { Localize, localize } from '@deriv/translations';
 import { hasCallPutEqual, hasDurationForCallPutEqual } from 'Stores/Modules/Trading/Helpers/allow-equals';
 import { useTraderStore } from 'Stores/useTraderStores';
@@ -17,6 +17,7 @@ const AllowEquals = observer(({ is_minimized }: TAllowEqualsProps) => {
 
     const [is_open, setIsOpen] = React.useState(false);
     const [is_allow_equal_enabled, setIsAllowEqualEnabled] = React.useState(!!is_equal);
+    const [current_index, setCurrentIndex] = React.useState(0);
 
     const has_callputequal_duration = hasDurationForCallPutEqual(
         contract_types_list,
@@ -26,17 +27,16 @@ const AllowEquals = observer(({ is_minimized }: TAllowEqualsProps) => {
     const has_callputequal = hasCallPutEqual(contract_types_list);
     const has_allow_equals = (has_callputequal_duration || expiry_type === 'endtime') && has_callputequal;
 
-    const onInfoIconClick = () => {
-        // console.log('Info Icon clicked!');
-    };
+    const onInfoIconClick = () => setCurrentIndex(1);
+    const onArrowIconClick = () => setCurrentIndex(0);
     const onSaveButtonClick = () => {
         if (!!is_equal !== is_allow_equal_enabled)
             onChange({ target: { name: 'is_equal', value: Number(is_allow_equal_enabled) } });
     };
-
     const onActionSheetClose = () => {
         setIsOpen(false);
         setIsAllowEqualEnabled(!!is_equal);
+        setCurrentIndex(0);
     };
 
     React.useEffect(() => {
@@ -57,28 +57,46 @@ const AllowEquals = observer(({ is_minimized }: TAllowEqualsProps) => {
             />
             <ActionSheet.Root isOpen={is_open} onClose={onActionSheetClose} position='left' expandable={false}>
                 <ActionSheet.Portal shouldCloseOnDrag>
-                    <ActionSheet.Header
-                        title={<Localize i18n_default_text='Allow equals' />}
-                        icon={<LabelPairedCircleInfoMdRegularIcon onClick={onInfoIconClick} />}
-                    />
-                    <ActionSheet.Content className='trade-param__wrapper'>
-                        <div className='trade-param__content'>
-                            <Text>
-                                <Localize i18n_default_text='Allow equals' />
-                            </Text>
-                            <ToggleSwitch
-                                checked={is_allow_equal_enabled}
-                                onChange={(is_enabled: boolean) => setIsAllowEqualEnabled(is_enabled)}
+                    <ul className='carousel'>
+                        <li className='carousel__item' style={{ transform: `translateX(-${current_index * 100}%)` }}>
+                            <ActionSheet.Header
+                                title={<Localize i18n_default_text='Allow equals' />}
+                                icon={<LabelPairedCircleInfoMdRegularIcon onClick={onInfoIconClick} />}
                             />
-                        </div>
-                    </ActionSheet.Content>
-                    <ActionSheet.Footer
-                        alignment='vertical'
-                        primaryAction={{
-                            content: <Localize i18n_default_text='Save' />,
-                            onAction: onSaveButtonClick,
-                        }}
-                    />
+                            <ActionSheet.Content className='trade-param__wrapper'>
+                                <div className='trade-param__content'>
+                                    <Text>
+                                        <Localize i18n_default_text='Allow equals' />
+                                    </Text>
+                                    <ToggleSwitch
+                                        checked={is_allow_equal_enabled}
+                                        onChange={(is_enabled: boolean) => setIsAllowEqualEnabled(is_enabled)}
+                                    />
+                                </div>
+                            </ActionSheet.Content>
+                            <ActionSheet.Footer
+                                alignment='vertical'
+                                primaryAction={{
+                                    content: <Localize i18n_default_text='Save' />,
+                                    onAction: onSaveButtonClick,
+                                }}
+                            />
+                        </li>
+                        <li className='carousel__item' style={{ transform: `translateX(-${current_index * 100}%)` }}>
+                            <ActionSheet.Header
+                                title={<Localize i18n_default_text='Allow equals' />}
+                                icon={<LabelPairedArrowLeftMdRegularIcon onClick={onArrowIconClick} />}
+                                className='icon--left'
+                            />
+                            <ActionSheet.Content className='trade-param__wrapper--definition'>
+                                <div className='trade-param__content'>
+                                    <Text>
+                                        <Localize i18n_default_text='Win payout if exit spot is also equal to entry spot.' />
+                                    </Text>
+                                </div>
+                            </ActionSheet.Content>
+                        </li>
+                    </ul>
                 </ActionSheet.Portal>
             </ActionSheet.Root>
         </React.Fragment>

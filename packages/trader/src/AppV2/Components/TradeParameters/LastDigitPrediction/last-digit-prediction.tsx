cf0d5e7abf7bd79c89782012612aa6a9fb4f850f
maryia-deriv <maryia@binary.com>
2024-07-18 11:38:24 +0300

    feat: LastDigitPrediction stats mode

--- packages/trader/src/AppV2/Components/TradeParameters/LastDigitPrediction/last-digit-prediction.tsx
+++ packages/trader/src/AppV2/Components/TradeParameters/LastDigitPrediction/last-digit-prediction.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import clsx from 'clsx';
-import { CaptionText, Text, TextField } from '@deriv-com/quill-ui';
+import { CaptionText, TextField } from '@deriv-com/quill-ui';
 import { Localize, localize } from '@deriv/translations';
 import { useTraderStore } from 'Stores/useTraderStores';
+import Digit from './digit';
 
 type TLastDigitSelectorProps = {
     is_minimized?: boolean;
+    is_stats_mode?: boolean;
 };
 
 const display_array = [...Array(10).keys()]; // digits array [0 - 9]
 
-const LastDigitPrediction = observer(({ is_minimized }: TLastDigitSelectorProps) => {
+const LastDigitPrediction = observer(({ is_minimized, is_stats_mode }: TLastDigitSelectorProps) => {
     const { digit_stats, last_digit, onChange } = useTraderStore();
 
     const handleLastDigitChange = (digit: number) => {
@@ -29,29 +31,27 @@ const LastDigitPrediction = observer(({ is_minimized }: TLastDigitSelectorProps)
             />
         );
     return (
-        <div className='last-digit-prediction'>
-            <CaptionText size='sm' className='last-digit-prediction__title'>
-                <Localize i18n_default_text='Last digit prediction' />
-            </CaptionText>
+        <div className={clsx('last-digit-prediction', is_stats_mode && 'last-digit-prediction--stats-mode')}>
+            {!is_stats_mode && (
+                <CaptionText size='sm' className='last-digit-prediction__title'>
+                    <Localize i18n_default_text='Last digit prediction' />
+                </CaptionText>
+            )}
             <div className='last-digit-prediction__selector'>
-                {display_array.map(digit => {
-                    const stats = digit_stats.length ? digit_stats[digit] : null;
-                    const percentage = stats ? (stats * 100) / 1000 : null;
-                    const display_percentage =
-                        percentage && !isNaN(percentage) ? parseFloat(percentage.toFixed(1)) : null;
-                    return (
-                        <div key={digit} className={clsx('digit', last_digit === digit && 'digit--active')}>
-                            <button onClick={() => handleLastDigitChange(digit)} name='last_digit'>
-                                <Text size='xl'>{digit}</Text>
-                            </button>
-                            {!!display_percentage && (
-                                <CaptionText size='sm' className='percentage'>
-                                    {display_percentage}%
-                                </CaptionText>
-                            )}
-                        </div>
-                    );
-                })}
+                {[...Array(2).keys()].map(row_key => (
+                    <div key={row_key} className='last-digit-prediction__selector-row'>
+                        {display_array.slice(row_key * 5, (row_key + 1) * 5).map(digit => (
+                            <Digit
+                                key={digit}
+                                digit={digit}
+                                digit_stats={digit_stats}
+                                is_active={!is_stats_mode && last_digit === digit}
+                                is_disabled={is_stats_mode}
+                                onClick={handleLastDigitChange}
+                            />
+                        ))}
+                    </div>
+                ))}
             </div>
         </div>
     );
